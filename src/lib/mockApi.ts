fe8063755761b7163313508309dbdfdaf1d2c69d
ishathameemaa <ishathameemaa@gmail.com>
2025-10-05 14:12:34 +0530

    commit

--- src/lib/mockApi.ts
+++ src/lib/mockApi.ts
@@ -1,4 +1,10 @@
-export async function mockLogin(data: any) {
+
+interface LoginData {
+  email: string;
+  password: string;
+}
+
+export async function mockLogin(data) {
   const res = await fetch("http://localhost:5000/api/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
