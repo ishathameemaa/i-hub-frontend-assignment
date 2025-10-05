a63f3fcedebb9b746458257c6c490ed37bd965cc
ishathameemaa <ishathameemaa@gmail.com>
2025-10-05 14:20:29 +0530

    mockapi

--- src/lib/mockApi.ts
+++ src/lib/mockApi.ts
@@ -1,20 +1,14 @@
-
 interface LoginData {
   email: string;
   password: string;
 }
 
-export async function mockLogin(data) {
+export async function mockLogin(data: LoginData) {
   const res = await fetch("http://localhost:5000/api/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(data),
   });
 
-  if (!res.ok) {
-    const error = await res.json();
-    throw new Error(error.error || "Login failed");
-  }
-
-  return res.json();
+  return await res.json();
 }
