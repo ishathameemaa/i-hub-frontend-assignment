51d24241dba6248c498bcc7b4127636c03cde2fe
ishathameemaa <ishathameemaa@gmail.com>
2025-10-05 14:09:31 +0530

    dfdf

--- src/lib/mockApi.ts
+++ src/lib/mockApi.ts
@@ -1,5 +1,4 @@
-// src/lib/mockApi.js
-export async function mockLogin(data) {
+export async function mockLogin(data: any) {
   const res = await fetch("http://localhost:5000/api/login", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
